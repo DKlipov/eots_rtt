313a915edeb5ca98afb7ed11045866885318ded2
Daniil Klipov <danklipov97@gmail.com>
2026-09-24 13:01:26 +0300

    Sherlock mode update

--- rules.js
+++ rules.js
@@ -17449,14 +17449,14 @@ function create_view(sherlock) {
     })
 
 
-    if (sherlock || R !== JP) {
+    if (!sherlock || R !== JP) {
         V.hand[JP] = G.hand[JP].length + G.offensive.draw[JP].length
     } else {
         V.hand[JP] = G.hand[JP].slice()
         G.offensive.draw[JP].forEach(c => V.hand[JP].push(c))
         V.future_offensive[JP] = G.future_offensive[JP]
     }
-    if (sherlock ||R !== AP) {
+    if (!sherlock ||R !== AP) {
         V.hand[AP] = G.hand[AP].length + G.offensive.draw[AP].length
     } else {
         V.hand[AP] = G.hand[AP].slice()
